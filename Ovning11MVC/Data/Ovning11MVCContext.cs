e29f9f60736172b8a2c5dd9437102a7cfb200ecb
Mikael <test.kurs@hotmail.net>
2019-12-09 09:22:04 -0800

    Add project files.

---
+++ Ovning11MVC/Data/Ovning11MVCContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ovning11MVC.Models
+{
+    public class Ovning11MVCContext : DbContext
+    {
+        public Ovning11MVCContext (DbContextOptions<Ovning11MVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ovning11MVC.Models.Product> Product { get; set; }
+    }
+}
